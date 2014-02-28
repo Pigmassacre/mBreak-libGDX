2ec115e40a851fbb3519ed6f7d6ab2079b392d6f
Pigmassacre <gulligaolle@gmail.com>
2014-02-28 16:31:11 +0100

    Added icon and fixed stuff

--- mBreak-android/src/com/pigmassacre/mbreak/AndroidGame.java
+++ mBreak-android/src/com/pigmassacre/mbreak/AndroidGame.java
@@ -1,11 +1,27 @@
 package com.pigmassacre.mbreak;
 
+import android.annotation.SuppressLint;
+import android.graphics.Point;
+import android.view.Display;
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
 
+@SuppressLint("NewApi")
 public class AndroidGame extends AndroidApplication {
     public void onCreate (android.os.Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        
+        Display display = getWindowManager().getDefaultDisplay();
+        int newScale;
+        if (android.os.Build.VERSION.SDK_INT >= 13) {
+        	Point size = new Point();
+            display.getSize(size);
+            newScale = size.x / Settings.BASE_SCREEN_WIDTH;
+        } else {
+        	newScale = display.getWidth() / Settings.BASE_SCREEN_WIDTH;
+        }
+        Settings.GAME_SCALE = newScale;
+        
         AndroidApplicationConfiguration cfg = new AndroidApplicationConfiguration();
         cfg.hideStatusBar = true;
         initialize(new MBreak(), cfg);
